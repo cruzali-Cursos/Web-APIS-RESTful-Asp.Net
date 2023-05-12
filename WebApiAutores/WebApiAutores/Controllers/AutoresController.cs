f7886ac6bb9aa0f54e6297686b33f75a92cc96bc
Ali <ali.cruz@me.com>
2023-05-12 07:24:03 -0600

    Fundamentos de Web API, Preparando el entorno, Construccion del primer web API, Controladores y Acciones, Ruteo, Tipo de dato de Retorno, Programacion Asincrona,

--- WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -5,7 +5,7 @@ using WebApiAutores.Entidades;
 namespace WebApiAutores.Controllers
 {
     [ApiController]
-    [Route("api/autores")]
+    [Route("api/[controller]")]  // Ruta Base, ruta del controlador
     public class AutoresController : ControllerBase
     {
         private readonly ApplicationDbContext context; // Ahora es accesible desde cualquier parte de esta clase
@@ -15,11 +15,59 @@ namespace WebApiAutores.Controllers
         }
 
 
-        [HttpGet]
-        public async Task<ActionResult<List<Autor>>> Get()
+        [HttpGet]   // api/Autores
+        [HttpGet("listado")] // api/Autores/listado
+        [HttpGet("/listado")] // listado
+        public async Task<List<Autor>> Get()
         {
             return await context.Autores.Include(x => x.Libros).ToListAsync();
         }
+
+        [HttpGet("primero")]  // api/autores/primero
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+
+        [HttpGet("primero2")]  // api/autores/primero
+        public ActionResult<Autor> PrimerAutor2()
+        {
+            return new Autor() { Nombre = "Ali" }; // Desde memoria.
+        }
+
+
+        // Se vuelve una plantilla, ya que primero pide un entero diagonal un parametro string.
+        // Se vuelve opcional si se le agrega el signo ?
+        //[HttpGet("{id:int}/{param2?}")] // Variable de ruta opcional.
+        [HttpGet("{id:int}/{param2=valorPorDefecto}")] // Variable de ruta con valor por defecto.
+        public async Task<ActionResult<Autor>> Get(int id, string param2)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+
 
         [HttpPost]
         public async Task<ActionResult> Post(Autor autor)
@@ -29,6 +77,7 @@ namespace WebApiAutores.Controllers
             return Ok();
         }
 
+        // Se concatena a la ruta base.
         [HttpPut("{id:int}")] // api/autores/algo
         public async Task<ActionResult> Put(Autor autor, int id)
         {
